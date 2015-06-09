64de778c74a5873732acbcb2c36360f7d0ca3ea1
Isaac Bigsby Trogdon <mr@dyss.net>
2015-06-09 10:53:02 -0400

    insignificant change

--- modules/custom/bcr_display/bcr_player/bcr_player.js
+++ modules/custom/bcr_display/bcr_player/bcr_player.js
@@ -6,43 +6,15 @@
 
     $('.region-persistent', context).once(function() {
 
-
-      /**
-       * original player control - doesn't work in Safari
-       */
-//      $("#jquery_jplayer_1").jPlayer({
-//        ready: function () {
-//          $(this).jPlayer("setMedia", {
-//            oga: "http://berlincommunityradio.out.airtime.pro:8000/berlincommunityradio_a"
-//          }).jPlayer("play");
-//        },
-//        ended: function (event) {
-//          $(this).jPlayer("play");
-//        },
-//        solution:"flash,html",
-//        swfPath: "js",
-//        supplied: "oga"
-//      });
-
-
       $("#jquery_jplayer_1").jPlayer({
         ready: function (event) {
           $(this).jPlayer("setMedia", {
             mp3:"http://berlincommunityradio.out.airtime.pro:8000/berlincommunityradio_a"
-//            mp3:"http://berlincommunityradio.out.airtime.pro:8000/berlincommunityradio_a"
           });
           $(this).jPlayer('play');
         },
         supplied: "mp3"
-//        supplied: "oga, mp3"
-//        swfPath: "../../../../../libraries/jPlayer/dist/jplayer",
-//        wmode: "window"
-//        solution:"flash",
       });
-
-
-
-
 
     });
 
